9480ec930fb923bcb5ad3c725ea17c4cfc732a8a
Diego <penaomardiego@gmail.com>
2023-08-30 12:59:15 -0300

    detail

--- src/components/CardCityDetail.jsx
+++ src/components/CardCityDetail.jsx
@@ -10,32 +10,44 @@ import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrow
 
 export default function CardCityDetail({ city }) {
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <Card
+    <>
+      <Box
         sx={{
-          width: "75vw",
-          m: 2,
-          backgroundColor: "transparent",
-          boxShadow: "none",
-          color: "white",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        {/*   <CardMedia
+        <Card
+          sx={{
+            width: { xs: "80vw", md: "50vw" },
+            m: 2,
+            backgroundColor: "transparent",
+            boxShadow: "none",
+            color: "white",
+          }}
+        >
+          {/*   <CardMedia
       component="img"
       sx={{ height: "50vh" }}
       src={city.photo}
       title={city.city}
     /> */}
-        <CardContent sx={{ backgroundColor: "transparent" }}>
-          <Typography variant="h6">{city.city}</Typography>
-          <Typography variant="subtitle2">{city.description}</Typography>
-        </CardContent>
-        <CardActions sx={{ display: "flex", justifyContent: "center" }}>
-          <Button endIcon={<KeyboardDoubleArrowDownIcon />} variant="contained">
-            View Itineraries
-          </Button>
-        </CardActions>
-      </Card>
-    </Box>
+          <CardContent sx={{ backgroundColor: "transparent" }}>
+            <Typography variant="h2" sx={{ m: 2 }}>
+              {city.city}
+            </Typography>
+            <Typography variant="subtitle1">{city.description}</Typography>
+          </CardContent>
+          <CardActions sx={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              endIcon={<KeyboardDoubleArrowDownIcon />}
+              variant="contained"
+            >
+              View Itineraries
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </>
   );
 }
